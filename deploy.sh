1e277ff089e1f96b2d582a414d374e095d08ba0c
DaVince <vincent.beers@gmail.com>
2024-11-29 20:15:35 +0100

    Fix files with spaces not processing

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ cd dist
 ../tools/pandoc-3.5/bin/pandoc -s -f markdown -t html5 -o index.html index.md -c style.css
 cd pages
 for file in **/*.md; do
-  ../../tools/pandoc-3.5/bin/pandoc -s -f markdown -t html5 -o $file.html $file -c ../../style.css
+  ../../tools/pandoc-3.5/bin/pandoc -s -f markdown -t html5 -o "$file.html" "$file" -c ../../style.css
 done
 
 echo "==============================="
